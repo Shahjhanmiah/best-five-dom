6d9d199f9ba40cefc1f0d15e9dc7ac3981713918
Shajham miah <mdshahjhan478877@gmail.com>
2022-08-23 06:28:34 +0600

    my totalAmount

--- balance.js
+++ balance.js
@@ -1,13 +1,48 @@
-document.getElementById('player-price').addEventListener('click', function(){
-    //console.log('player-price click');
+document.getElementById('calculate-player-expenses').addEventListener('click',function(){
+ const playerPrice=  document.getElementById('player-price');
+ const newPlayerPriceStr= playerPrice.value;
+ const newPlayerPrice= parseFloat(newPlayerPriceStr);
+ console.log(newPlayerPrice);
 
-     const playerPriceElement= document.getElementById('player-price');
-     const playertotal=6;
-     const onePlayerPrice= playertotal*1000;
-     const previousPlayerPriceString=playerPriceElement.ariaValueMax;
-     const previousPlayerPrice= parseFloat(previousPlayerPriceString);
-     const onePlayerPrice1= previousPlayerPrice*6;
-     console.log(onePlayerPrice1);
-    
+
+ const totalBalance= document.getElementById('total-balance');
+ const previousTotalBalanceStr= totalBalance.innerText;
+ const previousTotalBalance= parseFloat(previousTotalBalanceStr);
+ const totalPrice= newPlayerPrice*5;
+ totalBalance.innerText=totalPrice;
+ 
+
+
+
+})
+
+
+
+
+
+
+document.getElementById('calculate').addEventListener('click',function(){
+    console.log('calculate')
+
+
+    const managerSalary= document.getElementById('manager-salary');
+    const newManagerSalaryString= managerSalary.value;
+    const newManagerSalary= parseFloat(newManagerSalaryString);
+    console.log(newManagerSalary);
+
+    const coachSalary= document.getElementById('coach-salary');
+    const previousCoachSalaryString= coachSalary.value;
+    const previousCoachSalary= parseFloat(previousCoachSalaryString);
+    const previousCoachAmount= previousCoachSalary+newManagerSalary;
+    console.log(previousCoachAmount);
+
+
+    const totalExpensess=  document.getElementById('total-expensess');
+    const previousTotalExpensessString= totalExpensess.innerText;
+    const previousTotalExpensess= parseFloat(previousTotalExpensessString);
+    const totalAmount=previousCoachSalary+newManagerSalary;
+    totalExpensess.innerText=totalAmount;
+
+
 
 })
--- dom.js
+++ dom.js
@@ -5,13 +5,15 @@ document.getElementById('palyer-1').addEventListener('click', function(){
     mainContainer.appendChild(li1);
 
 
+    
+
 })
 document.getElementById('palyer-2').addEventListener('click', function(){
     const mainContainer = document.getElementById('main-container');
     const li1 = document.createElement('li');
     li1.innerText = '2.KaKa';
     mainContainer.appendChild(li1);
-
+    
 
 })
 document.getElementById('palyer-3').addEventListener('click', function(){
@@ -19,6 +21,7 @@ document.getElementById('palyer-3').addEventListener('click', function(){
     const li1 = document.createElement('li');
     li1.innerText = '3.Neymar jr';
     mainContainer.appendChild(li1);
+   
 
 
 })
@@ -28,6 +31,7 @@ document.getElementById('palyer-4').addEventListener('click', function(){
     const li1 = document.createElement('li');
     li1.innerText = '4.KylinaMbappe';
     mainContainer.appendChild(li1);
+   
 
 
 })
@@ -36,6 +40,7 @@ document.getElementById('palyer-5').addEventListener('click', function(){
     const li1 = document.createElement('li');
     li1.innerText = '5.RenatoSanches';
     mainContainer.appendChild(li1);
+    
 
 
 })
@@ -44,9 +49,37 @@ document.getElementById('palyer-6').addEventListener('click', function(){
     const li1 = document.createElement('li');
     li1.innerText = '6.SergioRamos';
     mainContainer.appendChild(li1);
+    
+
+
 
 
 })
+function myFunction1() {
+    document.getElementById("palyer-1").disabled = true;
+  }
+function myFunction2() {
+    document.getElementById("palyer-2").disabled = true;
+  }
+function myFunction3() {
+    document.getElementById("palyer-3").disabled = true;
+  }
+function myFunction4() {
+    document.getElementById("palyer-4").disabled = true;
+  }
+function myFunction5() {
+    document.getElementById("palyer-5").disabled = true;
+  }
+function myFunction6() {
+    document.getElementById("palyer-6").disabled = true;
+  }
+   
+
+
+
+  
+
+
 
 
 
